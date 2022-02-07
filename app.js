6b9a2ba381f46d329485e129342ab3e5761ccf4b
Parth sethi <sparth0200@gmail.com>
2022-02-08 02:00:22 +0530

    moved cors middleware to the up

--- app.js
+++ app.js
@@ -2,48 +2,34 @@ var createError = require('http-errors');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var db = require("./config/mongoose")
+var db = require('./config/mongoose');
 
 var apiRouter = require('./routes/api');
 
 var app = express();
-const cors = require("cors")
+const cors = require('cors');
 // view engine setup
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/api', apiRouter);
-app.use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Credentials', true)
-        res.setHeader('Access-Control-Allow-Origin', '*')
-// another common pattern
-// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-        res.setHeader(
-            'Access-Control-Allow-Headers',
-            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-        )
-        if (req.method === 'OPTIONS') {
-            res.status(200).end()
-        }
-        return next()
-    }
-)
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.sendStatus(500)
+	// render the error page
+	res.status(err.status || 500);
+	res.sendStatus(500);
 });
 
 module.exports = app;
